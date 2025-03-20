9c41d0fdc1b6d9ba9dce914b3bd119f991613c83
Jonathan Malmaud <malmaud@gmail.com>
2025-03-19 18:52:10 -0700

    add source

---
+++ src/poetry_demo/basic.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def get_mean(x1, x2):
+    return np.mean([x1, x2])
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,8 @@
 import pytest
 import numpy as np
+from poetry_demo import basic
 
 
 def test_basic():
-    x = np.mean([1.0, 3.0])
+    x = basic.get_mean(1.0, 3.0)
     assert x > 1.5
